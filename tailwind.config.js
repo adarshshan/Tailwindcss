f5f52692bca454af46a9609851d427bea7d335b6
Adarsh <adarshshanu3@gmail.com>
2024-02-03 07:39:45 +0530

    created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,29 @@
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.{html,js}","./public/js/*.js"],
+  content: ["./public/**/*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        papayawhip: {
+          light:'#fef4e4',
+          Default: '#ffefd5',
+          dark:'#fee5bc'
+        }
+      },
       screens: {
         'widescreen': { 'raw': '(min-aspect-ratio:3/2)' },
         'tallscreen': { 'raw': '(min-aspect-ratio:13/20)' }
       },
-      keyframes:{
-        'open-menu':{
-          '0%':{transform:'scaleY(0)'},
-          '80%':{transform:'scaleY(1.2)'},
-          '100%':{transform:'scaleY(1)'},
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '80%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
         }
       },
-      animation:{
-        'open-menu':'open-menu 0.5s ease-in-out forwards',
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
